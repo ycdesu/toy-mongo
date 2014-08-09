6a00ee850f1be9c21dccc21746c8bffc8a79dac8
jack <sychewo@gmail.com>
2014-08-09 12:09:10 +0800

    add InsertIndex controller

---
+++ src/main/java/org/yc/toymongo/rest/action/insert/InsertIndexAction.java
@@ -0,0 +1,16 @@
+package org.yc.toymongo.rest.action.insert;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * InsertIndex controller.
+ *
+ * @author ycavatars
+ */
+@RestController
+@EnableAutoConfiguration
+@RequestMapping(value = "/databases/{databaseName}/collections/{collectionName}/indexes")
+public class InsertIndexAction {
+}
